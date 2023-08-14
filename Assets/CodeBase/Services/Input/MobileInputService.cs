e07969e0810cad2391f68edee34e769992d57649
Stanislav Anishchenko <anischenko_stas@mail.ru>
2023-08-15 01:02:26 +0600

    Improved all non-characters materials.

--- Assets/CodeBase/Services/Input/MobileInputService.cs
+++ Assets/CodeBase/Services/Input/MobileInputService.cs
@@ -10,6 +10,26 @@ namespace CodeBase.Services.Input
 
         public override Vector2 MoveAxis => MoveSimpleInputAxis();
 
-        public override Vector2 LookAxis => LookSimpleInputAxis();
+        public override Vector2 LookAxis
+        {
+            get
+            {
+                Vector2 axis = LookSimpleInputAxis();
+
+                Debug.Log($"LookSimpleInputAxis: {axis}");
+
+                if (axis == Vector2.zero)
+                {
+                    axis = UnityAxis();
+
+                    Debug.Log($"UnityAxis: {axis}");
+                }
+
+                return axis;
+            }
+        }
+
+        private static Vector2 UnityAxis() =>
+            new(UnityEngine.Input.GetAxis(Horizontal2), UnityEngine.Input.GetAxis(Vertical2));
     }
 }
